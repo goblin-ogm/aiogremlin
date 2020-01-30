aafb04510b516c8ba3162555a0a4f5c442ba6e12
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2020-01-30 12:26:38 +0100

    Build: Updated setup.py to include more information.

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 import pip
 from setuptools import setup
 
+__author__ = 'Jeffrey Phillips Freeman'
+__maintainer__ = __author__
+__email__ = 'Jeffrey.Freeman@CleverThis.com'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2017, CleverThis, Inc. and contributors'
+__credits__ = ['David M. Brown - Project founder']
 
 setup(
     name='aiogremlin',
     version='3.3.2',
     url='',
-    license='Apache Software License',
-    author='Jeffrey Phillips Freeman',
-    author_email='Jeffrey.Freeman@CleverThis.com',
-    description='Async Gremlin-Python',
+    license=__license__,
+    author=__author__,
+    author_email=__email__,
+    description='An asynchronous DSL for the Gremlin-Python driver',
     long_description=open('README.md').read(),
+    url='http://goblin-ogm.com',
+    download_url='https://github.com/goblin-ogm/aiogremlin/archive/v3.3.2.tar.gz',
+    include_package_data=True,
+    keywords=['Tinkerpop', 'Tinkerpop3', 'gremlin', 'gremlin-python', 'asyncio', 'graphdb'],
     packages=['aiogremlin',
               'aiogremlin.driver',
               'aiogremlin.driver.aiohttp',
@@ -34,6 +44,14 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        # uncomment if you test on these interpreters:
+        # 'Programming Language :: Python :: Implementation :: IronPython',
+        # 'Programming Language :: Python :: Implementation :: Jython',
+        # 'Programming Language :: Python :: Implementation :: Stackless',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ]
 )
